15a20c51e9aec915421ddc7c7ac6de9c8651d1ac
keshavrkaranth <keshavarkarantha@gmail.com>
2021-05-13 16:56:18 +0530

    Completed BMI caluclator app

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -1,8 +1,11 @@
+import 'package:bmi_calculator/main.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reUsablecard.dart';
 import 'iconsData.dart';
+import 'resultPage.dart';
+import 'caluclate.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -14,6 +17,9 @@ class _InputPageState extends State<InputPage> {
   Color femaleColour;
   Color activeColour = Color(0xFF1D1E33);
   Color inactiveColour = Color(0xFF111328);
+  int height = 180;
+  int weight = 50;
+  int age = 15;
   @override
   void initState() {
     super.initState();
@@ -56,6 +62,28 @@ class _InputPageState extends State<InputPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
+        actions: [
+          FlatButton(
+            onPressed: () {
+              BmiCaluclator calc =
+                  BmiCaluclator(height: height, weight: weight);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Resultpage(
+                    bmiResult: calc.CaluclateBmi(),
+                    interpretation: calc.getInterpretation(),
+                    ResultText: calc.GetResult(),
+                  ),
+                ),
+              );
+            },
+            child: Text(
+              "Caluclate",
+              style: TextStyle(color: Colors.blue, fontSize: 18.0),
+            ),
+          ),
+        ],
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -72,7 +100,7 @@ class _InputPageState extends State<InputPage> {
                     child: ReusableCard(
                       color: maleColour,
                       cardChild: customWidget(
-                        name: 'Male',
+                        name: 'MALE',
                         icn: FontAwesomeIcons.mars,
                       ),
                     ),
@@ -87,7 +115,7 @@ class _InputPageState extends State<InputPage> {
                     child: ReusableCard(
                       color: femaleColour,
                       cardChild: customWidget(
-                          name: 'Female', icn: FontAwesomeIcons.venus),
+                          name: 'FEMALE', icn: FontAwesomeIcons.venus),
                     ),
                   ),
                 ),
@@ -97,6 +125,58 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: ReusableCard(
               color: Color(0xFF1D1E33),
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                // crossAxisAlignment: CrossAxisAlignment.stretch,
+                // textBaseline: TextBaseline.alphabetic,
+                children: [
+                  Text(
+                    "HEIGHT",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        height.toString(),
+                        style: TextStyle(
+                            fontSize: 50.0, fontWeight: FontWeight.w900),
+                      ),
+                      Text(
+                        'cm',
+                        style:
+                            TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98)),
+                      ),
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                      activeTrackColor: Colors.white,
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x29EB1555),
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.0),
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 120,
+                      max: 220,
+                      onChanged: (double value) {
+                        setState(() {
+                          height = value.round();
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           Expanded(
@@ -105,25 +185,138 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     color: Color(0xFF1D1E33),
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'WEIGHT',
+                          style: TextStyle(
+                              fontSize: 18.0, color: Color(0xFF8D8E98)),
+                        ),
+                        Text(
+                          weight.toString(),
+                          style: TextStyle(
+                              fontSize: 50.0, fontWeight: FontWeight.w900),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            RoundIconButton(
+                                icon: FontAwesomeIcons.minus,
+                                onPressed: () {
+                                  setState(() {
+                                    weight--;
+                                  });
+                                }),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            RoundIconButton(
+                                icon: FontAwesomeIcons.plus,
+                                onPressed: () {
+                                  setState(() {
+                                    weight++;
+                                  });
+                                }),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     color: Color(0xFF1D1E33),
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'AGE',
+                          style: TextStyle(
+                              fontSize: 18.0, color: Color(0xFF8D8E98)),
+                        ),
+                        Text(
+                          age.toString(),
+                          style: TextStyle(
+                              fontSize: 50.0, fontWeight: FontWeight.w900),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            RoundIconButton(
+                              icon: FontAwesomeIcons.minus,
+                              onPressed: () {
+                                setState(() {
+                                  age--;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            RoundIconButton(
+                              icon: FontAwesomeIcons.plus,
+                              onPressed: () {
+                                setState(() {
+                                  age++;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
             ),
           ),
           // Container(
+          //   child: GestureDetector(
+          //     child: Text(
+          //       "Caluclate your BMI",
+          //       style: TextStyle(fontSize: 20.0, color: Colors.white),
+          //     ),
+          //     onPressed: () {
+          //       BmiCaluclator calc =
+          //           BmiCaluclator(height: height, weight: weight);
+          //       Navigator.push(
+          //           context,
+          //           MaterialPageRoute(
+          //               builder: (context) => Resultpage(
+          //                     bmiResult: calc.CaluclateBmi(),
+          //                     interpretation: calc.getInterpretation(),
+          //                     ResultText: calc.GetResult(),
+          //                   )));
+          //     },
+          //   ),
           //   height: 80.0,
           //   width: double.infinity,
           //   color: Color(0xFFEB1555),
           //   margin:
           //       EdgeInsets.only(top: 10.0, left: 5.0, right: 5.0, bottom: 10.0),
-          // )
+          // ),
         ],
       ),
+    );
+  }
+}
+
+class RoundIconButton extends StatelessWidget {
+  RoundIconButton({@required this.icon, this.onPressed});
+
+  final IconData icon;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: onPressed,
+      elevation: 6.0,
+      child: Icon(icon),
+      constraints: BoxConstraints.tightFor(width: 56.0, height: 56.0),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
     );
   }
 }
